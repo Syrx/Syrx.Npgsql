1d13a9c351f1f26c7a8cc6fc6a495a917687f4c0
David Sexton <sextondjc@users.noreply.github.com>
2024-11-16 01:25:25 +0000

    Split from 2.1.0
    Split from 2.1.0 to pave the way to .NET 9 and easier maintenance

---
+++ tests/unit/Syrx.Commanders.Databases.Connectors.Npgsql.Extensions.Tests.Unit/NpgsqlDatabaseConnectorExtensionsTests/UsePostgres.cs
@@ -0,0 +1,26 @@
+﻿namespace Syrx.Commanders.Databases.Connectors.Npgsql.Extensions.Tests.Unit.NpgsqlDatabaseConnectorExtensionsTests
+{
+    public class UsePostgres
+    {
+        private IServiceCollection _services;
+
+        public UsePostgres()
+        {
+            _services = new ServiceCollection();
+        }
+
+        [Fact]
+        public void Successful()
+        {
+            _services.UseSyrx(a => a
+                .UsePostgres(b => b
+                    .AddCommand(c => c
+                        .ForType<UsePostgres>(d => d
+                            .ForMethod(nameof(Successful), e => e.UseCommandText("test-command").UseConnectionAlias("test-aliase"))))));
+
+            var provider = _services.BuildServiceProvider();
+            var connector = provider.GetService<IDatabaseConnector>();
+            IsType<NpgsqlDatabaseConnector>(connector);
+        }
+    }
+}
